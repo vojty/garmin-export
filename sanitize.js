8406d245e6dcd9ff0464b00620b4d71d62a20458
Tomáš Vojtášek <vojtasek.tomas@seznam.cz>
2017-11-27 21:08:54 +0100

    csv

--- sanitize.js
+++ sanitize.js
@@ -1,107 +1,87 @@
 const fs = require('fs');
 const path = require('path');
 const flatten = require('flat');
+const _ = require('lodash');
 
 const data = fs.readFileSync(path.join(__dirname, 'output/activities.json'));
 
 const json = JSON.parse(data);
 
-const dropKeysStarts = [
-    'activitySummary.SumSample',
-    'updatedDate',
-    'uploadDate',
-    'activitySummary.LossElevation',
-    'activitySummary.GainElevation',
-    'activityTimeZone',
-    'eventType',
-    'device',
-    'activityDescription',
-    'activityVideoUrl',
-    'locationName',
-    'isTitled',
-    'isElevationCorrected',
-    'isBarometricCapable',
-    'isSwimAlgorithmCapable',
-    'isVideoCapable',
-    'isActivityEdited',
-    'favorite',
-    'ispr',
-    'isAutoCalcCalories',
-    'isParent',
-    'parentId',
-    'uploadApplication.display',
-    'uploadApplication.key',
-    'uploadApplication.version',
-    'isDeviceReleased',
-    'externalId',
-    'privacy.display',
-    'privacy.key',
-    'privacy.fieldNameDisplay',
-    'numTrackpoints',
-    'totalLaps',
-    'garminSwimAlgorithm',
-    'userRoles',
-    'activitySummary.GainUncorrectedElevation',
-    'activitySummary.GainCorrectedElevation',
-    'activitySummary.LossUncorrectedElevation',
-    'activitySummary.EndLongitude',
-    'activitySummary.MinRunCadence',
-    'activitySummary.MinPace',
-    'activitySummary.MaxDoubleCadence',
-    'activitySummary.WeightedMeanDoubleCadence',
-    'activitySummary.BeginLatitude',
-    'activitySummary.MinSpeed',
-    'activitySummary.BeginLongitude',
-    'activitySummary.MaxCorrectedElevation',
-    'activitySummary.MaxUncorrectedElevation',
-    'activitySummary.MinUncorrectedElevation',
-    'activitySummary.LossCorrectedElevation',
-    'activitySummary.MinAirTemperature',
-    'activitySummary.MaxFractionalCadence',
-    'activitySummary.WeightedMeanFractionalCadence',
-    'activitySummary.MaxAirTemperature',
-    'activitySummary.EndLatitude',
-    'activitySummary.WeightedMeanAirTemperature',
-    'activitySummary.MinElevation',
-    'localizedSpeedLabel',
-    'localizedPaceLabel',
-    'activityType.parent',
-    'userId',
-    'username',
-    'activityType.fieldNameDisplay',
-    'activityType.display',
-    'displayname',
-    'activitySummary.MaxHeartRate.percentMax',
+const keys = [
+    'activityId',
+    'activityName',
+    'activityType.key',
+    'activitySummary.WeightedMeanMovingSpeed.value',
+    'activitySummary.WeightedMeanMovingPace.value',
+    'activitySummary.MaxHeartRate.value',
+    'activitySummary.WeightedMeanHeartRate.value',
+    'activitySummary.WeightedMeanHeartRate.percentMax.value',
+    'activitySummary.WeightedMeanHeartRate.zones.value',
+    'activitySummary.MaxSpeed.value',
+    'activitySummary.MaxPace.value',
+    'activitySummary.SumEnergy.value',
+    'activitySummary.MaxRunCadence.value',
+    'activitySummary.SumElapsedDuration.value',
+    'activitySummary.WeightedMeanRunCadence.value',
+    'activitySummary.SumMovingDuration.value',
+    'activitySummary.WeightedMeanSpeed.value',
+    'activitySummary.WeightedMeanPace.value',
+    'activitySummary.SumDuration.value',
+    'activitySummary.SumDistance.value',
+    'activitySummary.MinHeartRate.value',
+    'activitySummary.SumStep.value',
+    'activitySummary.BeginTimestamp.value',
+    'activitySummary.EndTimestamp.value',
+    'activitySummary.MaxVerticalSpeed.value',
+    'activitySummary.WeightedMeanStrideLength.value',
+    'activitySummary.MaxElevation.value',
+    'activitySummary.SumTrainingEffect.value',
+    'activitySummary.DirectVO2Max.value',
 ];
 
-const dropKeysEnds = [
-    'fieldDisplayName',
-    'display',
-    'displayUnit',
-    'withUnit',
-    'unitAbbr',
-    'withUnitAbbr',
-];
+const keyRenameMapping = {
+    activityId: 'Activity ID',
+    activityName: 'Activity name',
+    'activityType.key': 'Activity key',
+    'activitySummary.*': (key) => {
+        const [, property] = key.split('.');
+        return property.replace(/([A-Z])/g, ' $1').trim();
+    },
+};
 
-function isDroppingKey(key) {
-    return dropKeysStarts.some(d => key.startsWith(d)) ||
-        dropKeysEnds.some(d => key.endsWith(d));
+const valueTransformMapping = {
+};
+
+function transformValue(key, val) {
+    const transformation = Object.keys(valueTransformMapping).find(k => k === key);
+    if (transformation) {
+        return transformation(val);
+    }
+    return val;
 }
 
 function sanitizeKey(key) {
-    return key.replace(/^activitySummary\./, '');
+    const mappingKey = Object.keys(keyRenameMapping).find((k) => {
+        const r = new RegExp(k);
+        return r.test(key);
+    });
+    const mapping = keyRenameMapping[mappingKey];
+    if (mapping) {
+        return _.isFunction(mapping) ? mapping(key) : mapping;
+    }
+    return key;
 }
 
 const sanitized = json.map((a) => {
     const { activity } = a;
     const flat = flatten(activity);
     const filtered = Object.keys(flat).reduce((acc, k) => {
-        if (isDroppingKey(k)) {
+        if (!keys.includes(k)) {
             return acc;
         }
         return {
             ...acc,
-            [sanitizeKey(k)]: flat[k],
+            [sanitizeKey(k)]: transformValue(k, flat[k]),
         };
     }, {});
     return filtered;
